816d426d8e3cf32429ac64c4e0302dc2081b6912
JohnXD25 <jobietovera@gmail.com>
2023-01-19 10:12:54 +0100

    EnemyChase
    MAde range for enemy to chase player

--- Platform design/Assets/Scripts/EnemyFollowPlayer.cs
+++ Platform design/Assets/Scripts/EnemyFollowPlayer.cs
@@ -5,13 +5,24 @@ public class EnemyFollowPlayer : MonoBehaviour
 
     public float speed;
     private Transform player;
-   
+    public float lineOfSight;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
     void Update()
     {
-        transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed*Time.deltaTime);
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        if ( distanceFromPlayer < lineOfSight)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+        }
+       
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSight);
     }
 }
