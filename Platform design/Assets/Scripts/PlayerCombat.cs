411f915288e7aa250d246ba3e25da5bd6a5e78e4
SajjadLBS <sajjad.al-hilfi@elev.ga.lbs.se>
2023-01-20 12:04:02 +0100

    Combat script

--- Platform design/Assets/Scripts/PlayerCombat.cs
+++ Platform design/Assets/Scripts/PlayerCombat.cs
@@ -9,6 +9,7 @@ public class PlayerCombat : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
+    public int attackDamage = 40;
 
 
     // Update is called once per frame
@@ -31,6 +32,7 @@ public class PlayerCombat : MonoBehaviour
         //Damage them
         foreach(Collider2D enemy in hitEnemies)
         {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
             Debug.Log("We hit" + enemy.name);
         }
     }
